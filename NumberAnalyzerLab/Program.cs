afe7de35f023e07f8c7555e94d1b4c9e8d1ee9fc
Rob Moore <moorer0523@gmail.com>
2024-05-01 21:41:13 -0400

    Adding Number Analyzer Lab

---
+++ NumberAnalyzerLab/Program.cs
@@ -0,0 +1,71 @@
+﻿//initialize public variable
+bool isEven;
+bool keepAlive = true;
+
+//start while loop to enable repeat attempts
+while (keepAlive)
+{
+
+    //Ask for name
+    Console.WriteLine("What is your name?");
+    string userName = Console.ReadLine();
+
+    Console.WriteLine($"Hi {userName}, pick a number between 1 and 100");
+    string userInputNumber = Console.ReadLine();
+
+    //check for valid input
+    int.TryParse(userInputNumber, out int userNumber);
+
+    //check odd or even
+    if (userNumber % 2 == 0)
+    {
+        isEven = true;
+    }
+    else
+    {
+        isEven = false;
+    }
+
+    //requirements for even
+    //2 - 24
+    //26 - 60
+    // < 60
+
+    //requirements for odd
+    // < 60
+    // > 60
+
+
+    if (userNumber >= 1 && userNumber <= 100)
+    {
+        //checks the bool to limit number of if/else statements
+        if (isEven)
+        {
+            if (userNumber >= 2 && userNumber <= 24)
+                Console.WriteLine("Even and less than 25");
+            else if (userNumber >= 25 && userNumber <= 60)
+                Console.WriteLine("Even and between 26 and 60 inclusive.");
+            else if (userNumber > 60)
+                Console.WriteLine($"Hey {userName}, you typed {userNumber}. Even and greater than 60");
+        }
+        else
+        {
+            if (userNumber < 60)
+                Console.WriteLine($"Hey {userName}, you typed {userNumber}. Odd and less than 60.");
+            else if (userNumber > 60)
+                Console.WriteLine($"Hey {userName}, you typed {userNumber}. Odd and greater than 60.");
+        }
+
+        //Repeat the program prompt
+        Console.WriteLine("You did it! Did you want to run it again? Y/N");
+        string repeatInput = Console.ReadLine();
+        if (repeatInput.ToLower().Contains("y")) ;
+        else
+        {
+            Console.WriteLine("Thanks for typing numbers.");
+            keepAlive = false;
+        }
+    }
+    else
+        Console.WriteLine($"That number is out of range, lets try again {userName}");
+}
