f79e3ed3dc803891e7c72c2b9f4f75ab1cb887a2
Rob Moore <moorer0523@gmail.com>
2024-05-19 20:47:42 -0400

    adding BasicStringProcessingLab

---
+++ BasicStringProcessingLab/Program.cs
@@ -0,0 +1,39 @@
+﻿
+SentenceSplit();
+ListDisplay();
+
+static void SentenceSplit()
+{
+    while (true)
+    {
+        Console.WriteLine("Enter a sentence:");
+        string[] userInput = Console.ReadLine().Split(" ");
+        foreach (string line in userInput)
+        {
+            Console.WriteLine($"{line}");
+        }
+        Console.WriteLine("Would you like to continue?");
+        if (Console.ReadLine().ToLower().Contains("n"))
+            break;
+    }
+    Console.WriteLine("Goodbye!");
+}
+static void ListDisplay()
+{
+    List<string> createdList = new List<string>();
+    while (true)
+    {
+        Console.WriteLine("Enter some text: ");
+        createdList.Add(Console.ReadLine());
+        Console.WriteLine("You've entered: ");
+        for (int i = 0; i < createdList.Count; i++)
+        {
+            Console.Write($" {createdList[i]}");
+        }
+
+        Console.WriteLine("Would you like to continue?");
+        if (Console.ReadLine().ToLower().Contains("n"))
+            break;
+    }
+    Console.WriteLine("Goodbye!");
+}
