913f214332796d8fe04d683cc4ae00288e207b2c
Moorer0523 <moorer0523@gmail.com>
2024-09-04 17:34:34 -0400

    updated to add API

--- donuts-lab/src/app/components/donuts/donuts.component.ts
+++ donuts-lab/src/app/components/donuts/donuts.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Donut } from '../../models/donut';
+import { DonutAPIService } from '../../services/donut/donut-api.service';
 
 @Component({
   selector: 'app-donuts',
@@ -9,4 +11,22 @@ import { Component } from '@angular/core';
 })
 export class DonutsComponent {
 
+  siteLists : Donut
+
+  constructor(private donutAPI: DonutAPIService) {}
+
+  
+  ngOnInit() {
+    this.donutAPI.getList().subscribe({
+      next: (data) => {
+        this.siteLists = data
+      },
+      error: (error) => {
+        console.log(error)
+      },
+      complete: () => {
+        console.log('complete')
+      }
+    })
+  }
 }
