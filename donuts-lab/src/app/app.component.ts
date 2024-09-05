e3ffdebd6c44452e717a1c8670f258edda548b54
Moorer0523 <moorer0523@gmail.com>
2024-09-05 09:54:08 -0400

    adding donutlab bootstrap

--- donuts-lab/src/app/app.component.ts
+++ donuts-lab/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { DonutsComponent } from './components/donuts/donuts.component';
+import { DonutCardComponent } from './components/donut-card/donut-card.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, DonutsComponent],
+  imports: [RouterOutlet, DonutsComponent, DonutCardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
